39d2393344368f81e7dadbc2af4d63c6386356c2
zhengwei <1666919036@gmail.com>
2019-12-09 08:35:45 +0000

    xml

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -33,7 +33,8 @@ class WxController extends Controller
     {
         $log_file = "wx.log";  //public
         //将接受的数据记录到日志文件
-        $data = date('Y-m-d','H:i:s') . json_encode($_POST);
+        $xml = file_get_contents("php://input");
+        $data = date('Y-m-d','H:i:s') . $xml;
         file_put_contents($log_file,$data,FILE_APPEND);    //追加写入
     }
 

86522b891d267650d6f31d2b3e76588ea5ea2956
zhengwei <1666919036@gmail.com>
2019-12-09 09:05:10 +0000

    dd

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::any('user/addUser','User\\LoginController@addUser');
-Route::any('wx','User\\WxController@index');
+Route::any('wx','WeiXin\\WxController@index');
 
 Route::any('weixin/token','WeiXin\\WxController@access_tonken');
 Route::any('weixin/userinfo','WeiXin\\WxController@getUserInfo');

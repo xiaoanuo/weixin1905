f5359bae450930c1afc8c71408b3e85a7bec9c35
zhengwei <1666919036@gmail.com>
2019-12-09 10:25:19 +0000

    接口

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -7,7 +7,26 @@ use Illuminate\Http\Request;
 
 class WxController extends Controller
 {
+    public function wx()
+    {
+        $token = 'nsdjsjsajvndsjk';
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr = $_GET['echostr'];
 
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            echo $echostr;
+        }else{
+            die("not ok");
+        }
+
+    }
     /**
      * 获取access_token
      */

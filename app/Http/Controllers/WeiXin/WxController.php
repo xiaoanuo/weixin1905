3aefe5535f8e51e4ea457e7a092d26ba8290c476
zhengwei <1666919036@gmail.com>
2019-12-09 18:49:45 +0000

    接口接入

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -9,7 +9,22 @@ class WxController extends Controller
 {
     public function wechat()
     {
+        $token = "2259b56f5898cd6192c";
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr = $_GET["echostr"];
 
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){      //验证通过
+            echo $echostr;
+        }else{
+            die("not ok");
+        }
     }
 
 }

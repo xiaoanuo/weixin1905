f5c0f639729ae2d92ca5bd83edac06f91a10a1ae
zhengwei <1666919036@gmail.com>
2019-12-10 00:28:43 +0000

    时间戳

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -35,9 +35,9 @@ class WxController extends Controller
      */
     public function receiv()
     {
-        $log_file = "wx.log";
+        $log_file = "wx.log";     //public
         //将接受的数据记录到日志文件
-        $data = json_encode($_POST);
+        $data = date('Y-m-d','H:i:s').json_encode($_POST);
         file_put_contents($log_file,$data,FILE_APPEND);     //追加写入
     }
 

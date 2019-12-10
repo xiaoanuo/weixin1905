edbcc364a8165d25fe1aa41a2703111673751e94
zhengwei <1666919036@gmail.com>
2019-12-10 02:13:25 +0000

    二次提交

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -64,7 +64,7 @@ class WxController extends Controller
         if($event == 'subscribe'){
             $openid = $xml_obj->FromUserName;          //获取用户的openid
             //获取用户信息
-            $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->>access_token.'&openid='.$openid.'&lang=zh_CN";
+            $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN";
             $user_info = file_get_contents($url);
             file_put_contents('wx_user.log',$user_info,FILE_APPEND);
         }

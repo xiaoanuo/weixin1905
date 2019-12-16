d686925870abbe2f12c30fa0707c429ae69a00bb
zhengwei <1666919036@gmail.com>
2019-12-16 23:55:56 +0000

    laravel

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\WeiXin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\WxUserModel;
+use Illuminate\Support\Facades\Redis;
 class WxController extends Controller
 {
     protected $access_token;
@@ -15,11 +16,25 @@ class WxController extends Controller
         $this->access_token = $this->getAccessToken();
     }
 
+    public function test()
+    {
+        echo $this->access_token;
+    }
+
     protected function getAccessToken()
     {
+        $key = 'wx_access_token';
+        $access_token = Redis::get($key);
+        if($access_token){
+            return $access_token;
+        }
+
         $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_SECRET');
         $data_json = file_get_contents($url);
         $arr = json_decode($data_json,true);
+
+        Redis::set($key,$arr['access_token']);
+        Redis::expire($key,3600);
         return $arr['access_token'];
     }
 
@@ -67,20 +82,21 @@ class WxController extends Controller
             $p = WxUserModel::where(['openid'=>$openid])->first();
             if($p){
                 $msg ="欢迎回家";
-               $xml = '<xml>
+                $xml = '<xml>
                           <ToUserName><![CDATA['.$openid.']]></ToUserName>
                           <FromUserName><![CDATA['.$xml_obj->fromUser.']]></FromUserName>
                           <CreateTime>'.time().'</CreateTime>
                           <MsgType><![CDATA[text]]></MsgType>
                           <Content><![CDATA['.$msg.']]></Content>
                         </xml>';
-               echo $xml;
+                echo $xml;
             }else{
                 //获取用户信息
                 $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$this->access_token.'&openid='.$openid.'&lang=zh_CN';
                 $user_info = file_get_contents($url);
                 $data = json_decode($user_info,true);
                 $nickname = $data['nickname'];
+
                 $user_data = [
                     'openid' => $openid,
                     'sub_time' => $xml_obj->CreateTime,
@@ -111,6 +127,8 @@ class WxController extends Controller
         $fromUser = $xml_obj->ToUserName;    //开发者ID
         $time = time();                           //time时间戳
 
+        $media_id = $xml_obj->MediaId;       //
+
         if($msg_type == 'text'){
             $content = date('Y-m-d H:i:s') . $xml_obj->Content;
             $respon_text = '<xml>
@@ -121,8 +139,60 @@ class WxController extends Controller
               <Content><![CDATA['.$content.']]></Content> 
             </xml>';
                 echo $respon_text;   //回复用户消息
+
+            //消息入库
+        }elseif($msg_type =='image'){  //图片消息
+                //下载图片
+            $this->getMedia2($media_id,$msg_type);
+                //回复图片
+
+        }elseif($msg_type == 'voice'){     //语音消息
+                //下载语音
+                $this->getMedia2($media_id,$msg_type);
+                //回复语音
+
         }
 
+    }
+
+
+
+    public function getMedia()
+    {
+        $MediaId = "vX6VRjqrTRnwCyPI699jmWZN_0gRb2uNWzDz9OQ6A01ZYlVVOKp7wl-YWgcNn8Iq";
+        $url = 'https://api.weixin.qq.com/cgi-bin/media/get?access_token='.$this->access_token.'&media_id='.$MediaId;
+        //获取素材内容
+        $data = file_get_contents($url);
+        //保存文件
+
+        $file_name = date('YmdHis').mt_rand(11111,99999).'.amr';
+        file_put_contents($file_name,$data);
+        echo "下载素材成功";echo '</br>';
+        echo "文件名：" .$file_name;
+    }
+
+
+    protected function getMedia2($media_id,$media_type)
+    {
+        $url = 'https://api.weixin.qq.com/cgi-bin/media/get?access_token='.$this->access_token.'&media_id='.$media_id;
+        //获取素材内容
+        $data = file_get_contents($url);
+        //获取文件扩展名
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $file_info = fifo_file($finfo,$data);    //返回一个文件信息
+        var_dump($file_info);die;
+        $extension = '.' . pathinfo($data)['extension'];
+        //保存文件
+        $save_path = 'wx_media/';
+        if($media_type=='image'){    //保存圖片
+            $file_name = date('YmdHis').mt_rand(11111,99999).$extension;
+            $save_path = $save_path . 'imgs/' . $file_name;
+        }elseif ($media_type=='voice'){
+            $file_name = date('YmdHis').mt_rand(11111,99999).'.amr';
+            $save_path = $save_path . 'voice/' .$file_name;
+        }
+
+        file_put_contents($file_name,$data);
     }
 
     /**
@@ -137,10 +207,5 @@ class WxController extends Controller
         file_put_contents($log_file,$json_str,FILE_APPEND);
     }
 
-
-    public function  ddd()
-    {
-        echo "小阿剑de大保健纳？？？";
-    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,30 @@
 |
 */
 
+//Route::get('/', function () {
+//    $file_name = "adc.mp3";
+//    $info = pathinfo(
+//        $file_name
+//    );
+//
+//    echo $file_name . '的文件扩展为：' . pathinfo($file_name)['extension'];die;
+//        echo '<pre>';print_r($info);echo '</pre>';die;
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    phpinfo();
 });
 
 Route::any('user/addUser','User\\LoginController@addUser');
 Route::get('cs/ddd','WeiXin\\WxController@ddd');
+Route::get('test/xml','WeiXin\\WxController@xmlTest');
+
+Route::get('cs/redis','WeiXin\\WxController@test');
 
 //微信开发
 Route::get('wx','WeiXin\\WxController@wechat');
 Route::post('wx','WeiXin\\WxController@receiv');   //接受微信的推送事件
-Route::get('test/xml','WeiXin\\WxController@xmlTest');
+Route::get('wx/media','WeiXin\\WxController@getMedia');    //获取图片
+
 
 

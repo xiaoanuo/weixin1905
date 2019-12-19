49c661edb172fea6c979666383a29680b70be973
zhengwei <1666919036@gmail.com>
2019-12-19 06:32:20 +0000

    显示头像人数

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -39,7 +39,7 @@ class VoteController extends Controller
         }
 
         $total = Redis::zCard($key);        // 获取总数
-        //echo '投票总人数： ' . $total;echo '</br>';
+        echo '投票总人数： ' . $total;echo '</br>';
         $members = Redis::zRange($key, 0, -1, true);       // 获取所有投票人的openid
         //echo '<pre>',print_r($members);echo'</pre>';echo '<hr>';
         foreach ($members as $k => $v) {
@@ -48,7 +48,7 @@ class VoteController extends Controller
             $u = Redis::hgetAll($u_k);
             //$u = Redis::hMget($u_k,['openid','nickname','sex','headimgurl']);
             //echo '<pre>',print_r($u);echo'</pre>';echo '<hr>';die;
-            echo '<img src="'.$u['headimgurl'].'">';echo '</br>';
+            echo '<img src="'.$u['headimgurl'].'">';
         }
     }
 

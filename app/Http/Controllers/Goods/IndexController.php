19474ecb64fb39658b0da54a9cee46a4826dbeb8
zhengwei <1666919036@gmail.com>
2019-12-20 09:30:09 +0000

    商品详情展示

--- app/Http/Controllers/Goods/IndexController.php
+++ app/Http/Controllers/Goods/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
+use App\Model\WxGoodsModel;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -10,8 +11,14 @@ class IndexController extends Controller
     /**商品详情页
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function detail()
+    public function detail(Request $request)
     {
-        return view('goods.detail');
+        $goods_id = $request->input('gid');
+        $goods = WxGoodsModel::find($goods_id);
+//        print_r($goods);
+        $data = [
+            'goods' => $goods
+        ];
+        return view('goods.detail',$data);
     }
 }

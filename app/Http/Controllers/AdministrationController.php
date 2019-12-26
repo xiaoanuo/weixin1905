2601ab7780b6b73c9b32eaac7cb901a45631de67
zhengwei <1666919036@gmail.com>
2019-12-26 01:41:53 +0000

    get

--- app/Http/Controllers/AdministrationController.php
+++ app/Http/Controllers/AdministrationController.php
@@ -15,7 +15,42 @@ class AdministrationController extends Controller
         $user_info = $this->getUserInfo($data['access_token'], $data['openid']);
         //保存用户信息
         $userinfo_key = 'h:u:'.$data['openid'];
-        dd($userinfo_key);
         Redis::hMset($userinfo_key,$user_info);
     }
+
+
+    /**
+     * 根据code获取access-token
+     * @param $code
+     * @return mixed
+     */
+    public function getAccessToken($code)
+    {
+        $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=' . env('WX_APPID') . '&secret=' . env('WX_SECRET') . '&code=' . $code . '&grant_type=authorization_code';
+        $json_data = file_get_contents($url);
+        return json_decode($json_data, true);
+    }
+
+
+    /**
+     * 获取用户基本信息
+     * @param $access_token
+     * @param $openid
+     * @return mixed
+     */
+    public function getUserInfo($access_token, $openid)
+    {
+        $user_url = 'https://api.weixin.qq.com/sns/userinfo?access_token=' . $access_token . '&openid=' . $openid . '&lang=zh_CN';
+        $json_data = file_get_contents($user_url);
+        $data = json_decode($json_data, true);
+//        echo '<pre>';print_r($data);echo '</pre>';die;
+        if (isset($data['errcode'])) {
+            // 错误处理
+            die('出错啦 40001');     //40001 标识：获取用户信息失败
+        }
+        return $data;   //返回用户信息
+
+    }
+
+
 }
